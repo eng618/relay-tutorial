7adcb88cad32cc948aa2362d8262c1c4cc59fee7
Eric N. Garcia <eng618@garciaericn.com>
2023-03-04 08:39:11 -0500

    feat: fetch query with Relay

---
+++ src/__generated__/AppRepositoryNameQuery.graphql.js
@@ -0,0 +1,94 @@
+/**
+ * @generated SignedSource<<8e920e3c1788b62d53bcaf2c86224b08>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+var node = (function(){
+var v0 = [
+  {
+    "kind": "Literal",
+    "name": "name",
+    "value": "relay-tutorial"
+  },
+  {
+    "kind": "Literal",
+    "name": "owner",
+    "value": "eng618"
+  }
+],
+v1 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "name",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "AppRepositoryNameQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v0/*: any*/),
+        "concreteType": "Repository",
+        "kind": "LinkedField",
+        "name": "repository",
+        "plural": false,
+        "selections": [
+          (v1/*: any*/)
+        ],
+        "storageKey": "repository(name:\"relay-tutorial\",owner:\"eng618\")"
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "AppRepositoryNameQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v0/*: any*/),
+        "concreteType": "Repository",
+        "kind": "LinkedField",
+        "name": "repository",
+        "plural": false,
+        "selections": [
+          (v1/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "id",
+            "storageKey": null
+          }
+        ],
+        "storageKey": "repository(name:\"relay-tutorial\",owner:\"eng618\")"
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "4a770ffb72213f33e9be2ee0b68ee4df",
+    "id": null,
+    "metadata": {},
+    "name": "AppRepositoryNameQuery",
+    "operationKind": "query",
+    "text": "query AppRepositoryNameQuery {\n  repository(owner: \"eng618\", name: \"relay-tutorial\") {\n    name\n    id\n  }\n}\n"
+  }
+};
+})();
+
+node.hash = "66db9c5c37ad3f64ade4a16f8b0d66ec";
+
+module.exports = node;
